3ac2ace49ccc9c8ea9c413e4f573b9e56b62a2c9
Arcam622 <jorge-6-19@hotmail.com>
2022-10-21 16:27:36 -0500

    cambios

--- app/Models/provincia.php
+++ app/Models/provincia.php
@@ -21,16 +21,16 @@ class Provincia extends Model
     protected $guarded =[
     ];
     public function scopeProvincias($query){
-        return $query->join('pais','pais.pais_id','=','provincia.pais_id')->where('empresa_id','=',Auth::user()->empresa_id)->where('provincia_estado','=','1')->orderBy('provincia_nombre','asc');
+        return $query->join('pais','pais.pais_id','=','provincia.pais_id')->where(function ($query){ $query->whereNull('empresa_id')->orwhere('empresa_id','=',Auth::user()->empresa_id); })->where('provincia_estado','=','1')->orderBy('provincia_nombre','asc');
     }
     public function scopeExiste($query, $id){
-        return $query->join('pais','pais.pais_id','=','provincia.pais_id')->where('empresa_id','=',Auth::user()->empresa_id)->where('provincia_nombre','=',$id);
+        return $query->join('pais','pais.pais_id','=','provincia.pais_id')->where(function ($query){ $query->whereNull('empresa_id')->orwhere('empresa_id','=',Auth::user()->empresa_id); })->where('provincia_nombre','=',$id);
     }
     public function scopeProvincia($query, $id){
-        return $query->join('pais','pais.pais_id','=','provincia.pais_id')->where('empresa_id','=',Auth::user()->empresa_id)->where('provincia_id','=',$id);
+        return $query->join('pais','pais.pais_id','=','provincia.pais_id')->where(function ($query){ $query->whereNull('empresa_id')->orwhere('empresa_id','=',Auth::user()->empresa_id); })->where('provincia_id','=',$id);
     }
     public function scopeProvinciaNombre($query, $nombre){
-        return $query->join('pais','pais.pais_id','=','provincia.pais_id')->where('empresa_id','=',Auth::user()->empresa_id)->where('provincia_nombre','=',$nombre);
+        return $query->join('pais','pais.pais_id','=','provincia.pais_id')->where(function ($query){ $query->whereNull('empresa_id')->orwhere('empresa_id','=',Auth::user()->empresa_id); })->where('provincia_nombre','=',$nombre);
     }
     public function pais()
     {
