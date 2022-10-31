237235a50ec13545cb9cde96130d6d9965f74ad4
Arcam622 <jorge-6-19@hotmail.com>
2022-10-31 12:21:31 -0500

    cambios

--- resources/views/admin/compras/xml/cargarxml.blade.php
+++ resources/views/admin/compras/xml/cargarxml.blade.php
@@ -1,12 +1,12 @@
 @extends ('admin.layouts.admin')
 @section('principal')
-<div class="card card-secondary">
+<div class="card card-secondary" style="position: absolute; width: 100%">
     <div class="card-header">
         <h3 class="card-title">Cargar Compras</h3>
     </div>
     <!-- /.card-header -->
     <div class="card-body">
-        <form class="form-horizontal" method="POST" action="{{ url("reporte/xml") }} " enctype="multipart/form-data"> 
+        <form id="idForm" class="form-horizontal" method="POST" action="{{ url("reporte/xml") }} " enctype="multipart/form-data"> 
         @csrf
             <div class="form-group row">
                 <label for="idDescripcion" class="col-sm-1 col-form-label"><center>Archivo SRI : </center></label>
@@ -62,4 +62,26 @@
     <!-- /.card-body -->
 </div>
 <!-- /.card -->
+
+<div id="div-gif" class="col-md-12 text-center" style="position: absolute;height: 300px; margin-top: 150px; display: none">
+    <img src="{{ url('img/loading.gif') }}" width=90px height=90px style="align-items: center">
+</div>
+<script>
+    function girarGif(){
+        document.getElementById("div-gif").style.display="inline"
+        console.log("girando")
+    }
+    function ocultarGif(){
+        document.getElementById("div-gif").style.display="none"
+        console.log("no girando")
+    }
+
+    setTimeout(function(){
+        console.log("registro de la funcion")
+        $("#idForm").submit(function(e) {
+            girarGif()
+            
+        });
+    }, 1200)
+</script>
 @endsection
